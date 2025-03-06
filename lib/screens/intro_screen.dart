3eb7b45b53481cbc8a8673e38a5ef1936e2b93c8
aqsabatool <aqsabatool2005@gmail.com>
2025-03-06 16:10:09 +0500

    Rewrite IntroScreen class and centered app title and slogan line

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'TrackX',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Oswald',
+              ),
+            ),
+            Text(
+              'Smart Tracking for Smarter Spending.',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
